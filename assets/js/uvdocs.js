20868b8d7340a2abd99e94419b8bb866c3191d6c
Piyush Goel <piyush.goel190@webkul.com>
2019-07-02 13:19:19 +0530

    sidebar tuning remaining

--- assets/js/uvdocs.js
+++ assets/js/uvdocs.js
@@ -225,15 +225,11 @@ function isInlineLink(link, domainName) {
 //close the search bar when user clicks on the body.
 window.onclick = function() {
 
-
     if(document.getElementById("results-container").hasChildNodes())
     {
         document.getElementById("search-bar-header").value = "";
-
         var ul = document.getElementById("results-container");
-      
         ul.innerHTML = "";
-        
     }
 
     if(document.getElementById("results-container-sidebar").hasChildNodes())
